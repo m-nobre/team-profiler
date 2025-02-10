edf3e200329ad1c8e0392b1665d483143ca8ecc7
Mário Nobre <m.nobre@ymail.com>
2025-02-10 03:44:26 +0000

    debug

--- src/TeamProfilerServiceProvider.php
+++ src/TeamProfilerServiceProvider.php
@@ -30,7 +30,7 @@ class TeamProfilerServiceProvider extends ServiceProvider
         //Register Routes
         $this->addRoutesToWeb();
 
-        $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
+        // $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
 
  
     }
@@ -38,97 +38,7 @@ class TeamProfilerServiceProvider extends ServiceProvider
     public function boot(): void
     {
         $this->checkProfilerTranslation();
-        // check if translation exists
-        // if (!strpos(strtolower(Lang::get(array_key_first(config('team-profiler.translations')))), strtolower(config('team-profiler.denomination')))) {
-        //                     $existing_translations = json_decode(file_get_contents(lang_path('en.json')), true) ?? false;
 
-        //                     dd(count($existing_translations).".".count(config('team-profiler.translations')));
-
-        // }
-            // if yes
-                // checks if it's team profiler
-                    // compare both by number of entries and keys
-                // checks teams key
-                    // if no teams key then it's custom file
-                        // save to merge later
-                    // if teams key
-                      
-
-        // // $this->addRoutesToWeb();
-        // // $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
-
-        // /* If "Manage Team" translation already exists in EN */
-
-        // if (!Lang::hasForLocale(array_key_first(config('team-profiler.translations')), 'en') || !strpos(strtolower(Lang::get(config('team-profiler.translations'))), strtolower(config('team-profiler.denomination')))) {
-
-
-        //     // if file exists we will parse json to keep current translations
-            
-        //     // try {
-
-        //     //     $lang_array = json_decode(file_get_contents(lang_path('en').".json"), true);
-
-        //     // } catch (\Throwable $th) {
-                
-        //     //     // TODO - deal with exception case it is not "file not found" and some other error
-        //     //     // is_array(array_key_exists(3, explode(":",$th->getMessage())))
-                
-        //     //     // File not found, we create  a empty array
-        //     //     $lang_array = array();
-
-        //     // }
-
-        //         // if no error, file exists, we need to;
-
-        //             // 1 loop through keys and compare with profiler ones, 
-        //                 // if existing then update with translated value from config
-        //                 // if not then append to array with translated value from config
-
-
-        //         $translated = array();
-
-        //         foreach (config('team-profiler.translations') as $key => $value) {
-
-        //             /* str_contains is case sensitive */
-                    
-        //             $translated[$key] = str_replace("Team", ucfirst(config('team-profiler.denomination')), str_replace("team", strtolower(config('team-profiler.denomination')), $key));
-
-
-        //         }
-                
-        //         // $existing_translations = json_decode(file_get_contents(lang_path().'en.json'), true) ?? false;
-        //         foreach ($translated as $key => $value) {
-        //             /* we would override anyway, so simplified by just updating case exists */
-        //             $lang_array[$key] = $value;
-        //         }
-        //             // 2. save lang file
-        //             $file = lang_path('vendor/team-profiler/en.json');
-                
-        //             File::ensureDirectoryExists(lang_path("vendor/team-profiler/"));
-
-        //             // if (Storage::exists($file)) {
-        //             //     if ($existing_translations) {
-        //             //         /* 
-        //             //         existing translations... possibilities:
-        //             //         - 
-        //             //         - user has changed denomination and there are routes with previous denomination
-        //             //         - user has defined some before instlling team profiler (it's not new app, then lower priority)
-        //             //         - both of the above, yet user installed with new app but added custom translations to file in the meantime, but now changed denomination, so in this case we need both
-
-        //             //         let's deal with some same time:
-        //             //         - let's find if there are any custom translations by checking both translation arrays against each other
-        //             //             - if there are, and denomination is different, then we need to merge them with new translation
-        //             //             - if there are not, and denomination exists and is the same, then nothing is needed
-        //             //         */
-        //             //     }
-        //             // }
-
-
-        //             file_put_contents($file, json_encode($lang_array, JSON_PRETTY_PRINT));
-
-        //             $this->loadTranslationsFrom(lang_path('vendor/team-profiler'));
-
-        // }
     }
 
     public function checkProfilerTranslation(): bool {
