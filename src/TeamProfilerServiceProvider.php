42a7f4f21d5bd096e0a542b7043528a28ea385af
Mário Nobre <m.nobre@ymail.com>
2025-02-10 14:32:50 +0000

    pre-package-uninstall

--- src/TeamProfilerServiceProvider.php
+++ src/TeamProfilerServiceProvider.php
@@ -169,13 +169,13 @@ class TeamProfilerServiceProvider extends ServiceProvider
         
         // not working :(
 
-        $user_routes = file_get_contents(base_path('routes/web.php'));
+        // $user_routes = file_get_contents(base_path('routes/web.php'));
 
-        $section_start = explode("/*TeamProfiler", $user_routes);
-        $section_end = explode("TeamProfiler*/", $section_start[1]);
-        $user_data = [$section_start[0], $section_end[1]];
-        $text = implode("\n", $user_data);
-        file_put_contents(base_path('routes/web.php'), $text.PHP_EOL);
+        // $section_start = explode("/*TeamProfiler", $user_routes);
+        // $section_end = explode("TeamProfiler*/", $section_start[1]);
+        // $user_data = [$section_start[0], $section_end[1]];
+        // $text = implode("\n", $user_data);
+        // file_put_contents(base_path('routes/web.php'), $text.PHP_EOL);
 
     }
 
