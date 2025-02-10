5300b5f8dfce69291108cf92538545cb8fa6e487
Mário Nobre <m.nobre@ymail.com>
2025-02-10 14:43:59 +0000

    pre package uninstall

---
+++ PrePackageUninstall.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace MNobre;
+
+class PrePackageUninstall
+{
+    public function __construct() {
+        
+        $user_routes = file_get_contents(base_path('routes/web.php'));
+
+        $section_start = explode("/*TeamProfiler", $user_routes);
+        $section_end = explode("TeamProfiler*/", $section_start[1]);
+        $user_data = [$section_start[0], $section_end[1]];
+        $text = implode("\n", $user_data);
+        file_put_contents(base_path('routes/web.php'), $text.PHP_EOL);
+    }
+}
