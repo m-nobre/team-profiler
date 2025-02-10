84f7ebfde00aafeb2b7b6c56effea79e96fb8b03
Mário Nobre <m.nobre@ymail.com>
2025-02-10 22:11:16 +0000

    pre package uninstall

--- src/Console/TeamProfilerUninstall.php
+++ src/Console/TeamProfilerUninstall.php
@@ -78,7 +78,10 @@ class TeamProfilerUninstall extends Command
                     
                     if (!empty($existing_translations[$key])) {
                         Log::info(strtolower($existing_translations[$key])." - ". strtolower($profilerDenomination));
-                        if (str_contains(strtolower($existing_translations[$key]), strtolower($profilerDenomination))) {
+                        if (
+                            str_contains(strtolower($existing_translations[$key]), strtolower($profilerDenomination)) ||
+                            str_starts_with(strtolower($existing_translations[$key]), strtolower($profilerDenomination))
+                            ) {
                             Log::info(strtolower($existing_translations[$key])." - ". strtolower($profilerDenomination));
                             unset($existing_translations[$key]);
                         }
